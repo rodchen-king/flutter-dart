a5192b104cdcf61a426ba07e71910a467917b013
rodchen <rodchen_king@outlook.com>
2021-11-03 21:13:36 +0800

    1. 初始化

---
+++ demo.dart
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: rodchen
+ * @Date: 2021-11-03 17:29:23
+ * @LastEditTime: 2021-11-03 17:36:28
+ * @LastEditors: rodchen
+ */
+void main() {
+  String str = "hellow";
+
+  print(str);
+}
